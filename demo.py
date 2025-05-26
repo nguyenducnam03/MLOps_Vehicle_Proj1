4415aa17293274f2807253d2406cf4e99b37f2d4
Duc Nam Nguyen <nguyenducnamk3@gmail.com>
2025-05-26 16:50:38 +0700

    End ingestion step:

--- demo.py
+++ demo.py
@@ -3,12 +3,19 @@
 # logging.debug("This is a debug message.")
 
 # Check the exception config
-from src.logger import logging
-from src.exception import MyException
-import sys
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
+
+# Run pipelien
+from src.logger import logging
+from src.pipline.training_pipeline import TrainPipeline
+
+pipeline = TrainPipeline()
+pipeline.run_pipeline() 
